39dea7e1c6fd7190485ebe2c6c961e2f2319c08a
Okabe-TO <ttt.sao.619@gmail.com>
2023-12-17 18:38:16 +0900

    [FIX] js/functions/signup.js: パラメータオブジェクト:credentialsの導入

--- js/functions/signup.js
+++ js/functions/signup.js
@@ -1,4 +1,4 @@
-import { auth, createUserWithEmailAndPassword } from './firebaseConfig.js';
+import { auth, createUserWithEmailAndPassword } from '../firebaseConfig.js';
 document.cookie = 'cookieName=value; SameSite=None; Secure';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,59 +8,65 @@ document.addEventListener('DOMContentLoaded', () => {
 	signupForm.addEventListener('submit', async (event) => {
 		event.preventDefault(); // フォームのデフォルトの送信を防止
 
-		let username = document.getElementById('username').value;
-		let password = document.getElementById('password').value;
+		const credentials = {
+			username: document.getElementById('username').value,
+			password: document.getElementById('password').value
+		};
 
-		submitValidation(username, password);
-		function submitValidation(username, password) {
-			// 入力値のバリデーション（例：E-mailの形式、Passwordの長さなど）
-			if (!username.includes('@')) {
-				errorMessage.textContent = '有効なE-mailアドレスを入力してください。';
-				return;
-			}
-			if (password.length < 6) {
-				errorMessage.textContent = 'パスワードは6文字以上で入力してください。';
-				return;
-			}
+		if (!submitValidation(credentials)) {
+			return;
 		}
 
 		try {
 			// ユーザ作成時にFirebase Authへ登録
-			const userCredential = await createUserWithEmailAndPassword(auth, username, password);
+			const userCredential = await createUserWithEmailAndPassword(auth, credentials.username, credentials.password);
 			// 送信成功時の処理
 			window.location.href = "signup-success.html"; // リダイレクト先のファイルを指定します
-
 		} catch (error) {
 			errorHandling(error);
-			function errorHandling(error) {
-				console.error("エラー:", error);
-				switch (error.code) {
-					case 'auth/email-already-in-use':
-						errorMessage.textContent = 'このE-mailアドレスはすでに使用されています。';
-						break;
-					case 'auth/invalid-email':
-						errorMessage.textContent = '無効なE-mailアドレスが入力されました。';
-						break;
-					case 'auth/operation-not-allowed':
-						errorMessage.textContent = 'この認証方法は現在利用できません。';
-						break;
-					case 'auth/weak-password':
-						errorMessage.textContent = 'パスワードが弱すぎます。もう一度確認してください。';
-						break;
-					case 'auth/user-disabled':
-						errorMessage.textContent = 'このアカウントは無効化されています。';
-						break;
-					case 'auth/user-not-found':
-						errorMessage.textContent = 'アカウントが存在しません。';
-						break;
-					case 'auth/wrong-password':
-						errorMessage.textContent = 'パスワードが間違っています。';
-						break;
-					default:
-						errorMessage.textContent = error.message; // その他のエラーメッセージを表示
-						break;
-				}
-			}
 		}
 	});
 });
+
+function submitValidation(credentials) {
+	// 入力値のバリデーション（例：E-mailの形式、Passwordの長さなど）
+	if (!credentials.username.includes('@')) {
+		errorMessage.textContent = '有効なE-mailアドレスを入力してください。';
+		return false;
+	}
+	if (credentials.password.length < 6) {
+		errorMessage.textContent = 'パスワードは6文字以上で入力してください。';
+		return false;
+	}
+	return true;
+}
+
+function errorHandling(error) {
+	console.error("エラー:", error);
+	switch (error.code) {
+		case 'auth/email-already-in-use':
+			errorMessage.textContent = 'このE-mailアドレスはすでに使用されています。';
+			break;
+		case 'auth/invalid-email':
+			errorMessage.textContent = '無効なE-mailアドレスが入力されました。';
+			break;
+		case 'auth/operation-not-allowed':
+			errorMessage.textContent = 'この認証方法は現在利用できません。';
+			break;
+		case 'auth/weak-password':
+			errorMessage.textContent = 'パスワードが弱すぎます。もう一度確認してください。';
+			break;
+		case 'auth/user-disabled':
+			errorMessage.textContent = 'このアカウントは無効化されています。';
+			break;
+		case 'auth/user-not-found':
+			errorMessage.textContent = 'アカウントが存在しません。';
+			break;
+		case 'auth/wrong-password':
+			errorMessage.textContent = 'パスワードが間違っています。';
+			break;
+		default:
+			errorMessage.textContent = error.message; // その他のエラーメッセージを表示
+			break;
+	}
+}
